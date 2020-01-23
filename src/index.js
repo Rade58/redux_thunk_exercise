22b45331fbfff65d95fcae8459e39ea30d474e6b
Rade58 <bajic.rade2@gmail.com>
2020-01-23 17:36:06 +0100

    wiring up reducer and setting up provider, creatingStore

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 //
 import reducer from './reducers/'
-console.log(reducer.toString())
+//
+import FetchItems from './components/FetchItems'
+import Items from './components/Items'
 //
 
 const root = document.getElementById('root')
 
+const store = createStore(reducer)
 
 
-
+const App = () => {
+  return (
+  <div className="App">
+    <h2>Items Stream</h2>
+    <FetchItems />
+    <Items />
+  </div>
+  )
+}
 
 
 ReactDom.render(
-  <div>some text</div>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   root
 )
